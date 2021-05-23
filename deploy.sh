7bd7b5c2c13716b7f85454ec7988aec506a4134c
protokeks <protokeks2@protonmail.com>
2021-05-23 20:39:20 +0400

    Embedded more software and CUPS profiles

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,12 @@ else
 fi
 
 if [[ $NVIDIA_GA == 1 ]]; then
-    printf "Installing Nvidia GPU Driver...\n"
+    printf 'Installing Nvidia GPU Driver...\n'
     sudo apt install -y linux-headers-$(uname -r)
     sudo apt install -y nvidia-384
 fi
 
-printf "Setting up configuration files...\n"
+printf 'Setting up configuration files...\n'
 
 if [[ $NVIDIA_GA == 1 ]]; then
     sudo cp -fv ./xorg.conf /etc/X11/
@@ -31,4 +31,29 @@ fi
 cp -fv ./beets.yaml ~/.config/beets/config.yaml
 cp -fv ./.aliasrc ~/
 
+sudo apt install -y zsh git wget software-properties-common apt-transport-https
+#sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+sudo add-apt-repository -y ppa:phoerious/keepassxc
+sudo add-apt-repository -y ppa:atareao/telegram
+
+sudo apt update
+sudo apt install -y keepassxc telegram qbittorrent firefox simple-scan qbittorrent
+
+wget "https://ftp.hp.com/pub/softlib/software13/printers/SS/SL-C4010ND/uld_V1.00.39_01.17.tar.gz" -O /tmp/uld_V1.00.39_01.17.tar.gz
+tar -xvf /tmp/uld_V1.00.39_01.17.tar.gz -C /tmp/
+sudo bash /tmp/uld/install-printer.sh
+
+wget http://security.ubuntu.com/ubuntu/pool/main/g/glibc/multiarch-support_2.27-3ubuntu1.2_amd64.deb -O /tmp/multiarch-support_2.27-3ubuntu1.2_amd64.deb
+sudo dpkg -i /tmp/multiarch-support_2.27-3ubuntu1.2_amd64.deb
+wget https://mirrors.edge.kernel.org/ubuntu/pool/universe/libg/libgnome-keyring/libgnome-keyring-common_3.12.0-1build1_all.deb -O /tmp/libgnome-keyring-common_3.12.0-1build1_all.deb
+sudo dpkg -i /tmp/libgnome-keyring-common_3.12.0-1build1_all.deb
+wget https://ftp.osuosl.org/pub/ubuntu/pool/universe/libg/libgnome-keyring/libgnome-keyring0_3.12.0-1build1_amd64.deb -O /tmp/libgnome-keyring0_3.12.0-1build1_amd64.deb
+sudo dpkg -i /tmp/libgnome-keyring0_3.12.0-1build1_amd64.deb
+sudo apt install -y libappindicator1 gconf2
+wget https://desktop.userapi.com/debian/pool/master/v/vk/vk_5.2.3-1_amd64.deb -O /tmp/vk_5.2.3-1_amd64.deb
+sudo dpkg -i /tmp/vk_5.2.3-1_amd64.deb
+
+sudo apt install -y libc++1 libc++1-10 libc++abi1-10
+wget -O /tmp/discord.deb "https://discord.com/api/download?platform=linux&format=deb"
+sudo dpkg -i /tmp/discord.deb
